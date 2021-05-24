fd44ce68e7ca23ee44996b6a5f9f799b7c8563f1
giordano <giordanovitag@libero.it>
2021-05-24 18:57:07 +0200

    esercizio completo

---
+++ laravel-migration-seeder/database/factories/MovieFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Movie;
+use Faker\Generator as Faker;
+
+$factory->define(Movie::class, function (Faker $faker) {
+    return [
+        'title' => $faker->firstname,
+        'original_title' => $faker->lastname,
+        'nationality' => $faker->stateAbbr,
+        'date' => $faker->date,
+        'vote' => $faker->numberBetween(1, 10),
+    ];
+});
